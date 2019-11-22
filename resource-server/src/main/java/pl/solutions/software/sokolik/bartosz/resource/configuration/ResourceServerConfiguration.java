18bac66453d0a74b5b5295cbfbb122f56c529b2c
Bartosz Sokolik <bsokolik@enzode.com>
2019-11-22 09:39:19 +0100

    Add EnableGlobalMethodSecurity annotation on ResourceServerConfiguration

--- resource-server/src/main/java/pl/solutions/software/sokolik/bartosz/resource/configuration/ResourceServerConfiguration.java
+++ resource-server/src/main/java/pl/solutions/software/sokolik/bartosz/resource/configuration/ResourceServerConfiguration.java
@@ -2,6 +2,7 @@ package pl.solutions.software.sokolik.bartosz.resource.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
@@ -9,6 +10,7 @@ import org.springframework.security.oauth2.provider.token.RemoteTokenServices;
 
 @Configuration
 @EnableResourceServer
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
 
     @Override
